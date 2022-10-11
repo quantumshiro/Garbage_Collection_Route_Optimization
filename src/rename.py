173a598b3dbcf16df101ff3779100608e85d91e4
quantumshiro <hiro.1nakanishi@gmail.com>
2022-10-11 11:31:17 +0900

    image padding

--- src/rename.py
+++ src/rename.py
@@ -1,6 +1,6 @@
 import os
 import glob
 
-tmp_dir = 'data/image/ngs'
+tmp_dir = 'data/image/ng'
 for i, file in enumerate(os.listdir(tmp_dir)):
     os.rename(tmp_dir + '/' + file, tmp_dir + '/' + str(i+1).zfill(6) + '.jpg')
